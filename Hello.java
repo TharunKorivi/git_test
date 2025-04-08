12b81844823572883e94a46800396827d7900115
TharunKorivi <tharun.korivi04@gmail.com>
2025-04-08 16:34:36 +0530

    Modified form

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(String arg[]) {
         System.out.println("Hello World!");
+        System.out.println("Form is chpublic class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Form is changed.");
+    }
+}");
+
     }
 }
