6f7a6c117a392c8489ac92f1bf3a765cd3e28c5b
TharunKorivi <tharun.korivi04@gmail.com>
2025-04-08 16:35:46 +0530

    Modified form

--- Hello.java
+++ Hello.java
@@ -1,12 +1,7 @@
 public class Hello {
     public static void main(String arg[]) {
         System.out.println("Hello World!");
-        System.out.println("Form is chpublic class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Form is changed.");
-    }
-}");
+        System.out.println("Form is ch");
 
     }
 }
